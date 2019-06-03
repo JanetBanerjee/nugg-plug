c958058bb071cfe04e63d70d3273aa9ae8ec498a
JanetBanerjee <banerjee.janet@gmail.com>
2019-06-03 16:16:40 -0400

    fixing user_type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :store_name, :area_code, :phone_1, :phone_2, :address, :city, :type) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :store_name, :area_code, :phone_1, :phone_2, :address, :city, :type) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :user_type) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :user_type) }
   end
 end
